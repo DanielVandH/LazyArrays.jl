6fd2bc82a290fe924e5b0b1ace2eacccc0e9fcc0
DanielVandH <danj.vandenheuvel@gmail.com>
2024-07-09 10:57:00 +0100

    Revert "Support transtype for DualLayout{ApplyLayout{typeof(*)}} (#335)"
    This reverts commit a7bd760b645266ff41303fca2d16d679379a6f57.

--- src/linalg/mul.jl
+++ src/linalg/mul.jl
@@ -368,7 +368,6 @@ simplify(M::Applied{<:Any,typeof(*)}) = simplify(*, arguments(M)...)
 @inline copy(M::Mul{<:DualLayout,<:LazyLayouts,<:AbstractMatrix,<:AbstractVector}) = copy(Dot(M))
 
 applylayout(::Type{typeof(*)}, ::DualLayout{Lay}, args...) where Lay = DualLayout{typeof(applylayout(typeof(*), Lay(), args...))}()
-transtype(A::MulMatrix) = transtype(first(A.args))
 
 #TODO: Why not all DiagonalLayout?
 @inline simplifiable(M::Mul{<:DiagonalLayout{<:AbstractFillLayout}}) = Val(true)
